1bef8a648dc32b6b922681a838e21c9a56391ac3
Rodrigo_Beleza <120214647+Iko2004@users.noreply.github.com>
2026-04-04 15:27:43 +0000

    Implementação das entidades Licenciatura e Professor.

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Licenciatura, Professor
+)
+
+@admin.register(Licenciatura)
+class LicenciaturaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ects_totais')
+    search_fields = ('nome', 'descricao')
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'numero', 'email')
+    search_fields = ('nome', 'numero', 'email')
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Licenciatura(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    ects_totais = models.IntegerField(default=180)
+    link_site = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+    numero = models.CharField(max_length=20, help_text="Número de docente") 
+    email = models.EmailField(blank=True, null=True)
+    link_lusofona = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
