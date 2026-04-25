9c76c9f30ba29554b4c5dc549fb20a58322f59ca
Rodrigo_Beleza <120214647+RodrigoBeleza10@users.noreply.github.com>
2026-04-25 01:22:22 +0000

    Criação da pagina html inicial do portfolio e a sua Navbar.

--- project/urls.py
+++ project/urls.py
@@ -16,20 +16,18 @@ Including another URLconf
 """
 
 ## ficheiro projects/urls.py
-
 from django.contrib import admin
 from django.urls import path, include              
 from django.conf import settings
 from django.conf.urls.static import static
 
-## project/urls.py
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("escola/", include("escola.urls")), 
-    path("", include("escola.urls")),  #  rota para app escola sem precisar de escrever "escola"
+
+    path('admin/', admin.site.urls),
+    
+
+    path('', include('portfolio.urls')), 
 ]
 
-# Serve ficheiros media em DEBUG
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
