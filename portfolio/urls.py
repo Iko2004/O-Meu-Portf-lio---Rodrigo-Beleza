08227f6cbaa56d79ab693edb3f6e6ca2193f5efb
Rodrigo_Beleza <120214647+RodrigoBeleza10@users.noreply.github.com>
2026-04-25 03:38:19 +0000

    Criação do html e das views das novas páginas do portfolio.

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('tecnologias/', views.tecnologias_view, name='tecnologias'),
+    path('percurso/', views.percurso_view, name='percurso'),
+    path('makingofs/', views.makingofs_view, name='makingofs'),
+    path('projetos/', views.projetos_view, name='projetos'),
+    path('contactos/', views.contactos_view, name='contactos'),
+    path('competencias/', views.competencias_view, name='competencias'),
+    path('tecnologias/eliminar/<int:id>/', views.eliminar_tecnologia, name='eliminar_tecnologia'),
+    path('tecnologias/editar/<int:id>/', views.editar_tecnologia, name='editar_tecnologia'),
+    path('projetos/eliminar/<int:id>/', views.eliminar_projeto, name='eliminar_projeto'),
+    path('projetos/editar/<int:id>/', views.editar_projeto, name='editar_projeto'),
+    path('percurso/editar/<int:id>/', views.editar_licenciatura, name='editar_licenciatura'),
+    path('percurso/eliminar/<int:id>/', views.eliminar_licenciatura, name='eliminar_licenciatura'),
+    path('percurso/eliminar-formacao/<int:id>/', views.eliminar_formacao, name='eliminar_formacao'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,131 @@
 from django.shortcuts import render
+from .models import Tecnologia 
+from .models import Licenciatura, Formacao
+from .models import Tecnologia, Licenciatura, Formacao, MakingOf 
+from .models import Projeto
+from .models import Competencia 
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Tecnologia
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Projeto
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Licenciatura, Formacao
+
 
 def home_view(request):
     return render(request, 'portfolio/home.html')
+
+
+def tecnologias_view(request):
+    tecnologias = Tecnologia.objects.all() 
+    return render(request, 'portfolio/tecnologias.html', {'tecnologias': tecnologias})
+
+
+def percurso_view(request):
+    licenciaturas = Licenciatura.objects.all()
+    formacoes = Formacao.objects.all()
+    return render(request, 'portfolio/percurso.html', {
+        'licenciaturas': licenciaturas,
+        'formacoes': formacoes
+    })
+
+
+def makingofs_view(request):
+    makingofs = MakingOf.objects.all().order_by('-id') 
+    return render(request, 'portfolio/makingofs.html', {'makingofs': makingofs})
+
+
+def projetos_view(request):
+    projetos = Projeto.objects.all()
+    return render(request, 'portfolio/projetos.html', {'projetos': projetos})
+
+def contactos_view(request):
+    return render(request, 'portfolio/contactos.html')
+
+
+def competencias_view(request):
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        nivel = request.POST.get('nivel')
+        Competencia.objects.create(nome=nome, nivel=nivel)
+        return redirect('competencias')
+        
+    competencias = Competencia.objects.all()
+    return render(request, 'portfolio/competencias.html', {'competencias': competencias})
+
+
+
+
+def tecnologias_view(request):
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        Tecnologia.objects.create(nome=nome)
+        return redirect('tecnologias')
+    
+    tecnologias = Tecnologia.objects.all()
+    return render(request, 'portfolio/tecnologias.html', {'tecnologias': tecnologias})
+
+def eliminar_tecnologia(request, id):
+    tecnologia = get_object_or_404(Tecnologia, id=id)
+    tecnologia.delete()
+    return redirect('tecnologias')
+
+def editar_tecnologia(request, id):
+    tecnologia = get_object_or_404(Tecnologia, id=id)
+    if request.method == "POST":
+        tecnologia.nome = request.POST.get('nome')
+        tecnologia.save()
+        return redirect('tecnologias')
+    return render(request, 'portfolio/editar_tecnologia.html', {'tecnologia': tecnologia})
+
+
+
+
+def projetos_view(request):
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        Projeto.objects.create(nome=nome, descricao=descricao)
+        return redirect('projetos')
+    
+    projetos = Projeto.objects.all()
+    return render(request, 'portfolio/projetos.html', {'projetos': projetos})
+
+def eliminar_projeto(request, id):
+    projeto = get_object_or_404(Projeto, id=id)
+    projeto.delete()
+    return redirect('projetos')
+
+def editar_projeto(request, id):
+    projeto = get_object_or_404(Projeto, id=id)
+    if request.method == "POST":
+        projeto.nome = request.POST.get('nome')
+        projeto.descricao = request.POST.get('descricao')
+        projeto.save()
+        return redirect('projetos')
+    return render(request, 'portfolio/editar_projeto.html', {'projeto': projeto})
+
+
+
+
+def eliminar_licenciatura(request, id):
+    licenciatura = get_object_or_404(Licenciatura, id=id)
+    licenciatura.delete()
+    return redirect('percurso')
+
+def eliminar_formacao(request, id):
+    formacao = get_object_or_404(Formacao, id=id)
+    formacao.delete()
+    return redirect('percurso')
+
+def editar_licenciatura(request, id):
+    licenciatura = get_object_or_404(Licenciatura, id=id)
+    if request.method == "POST":
+        licenciatura.nome = request.POST.get('nome')
+        licenciatura.descricao = request.POST.get('descricao')
+        licenciatura.save()
+        return redirect('percurso')
+    return render(request, 'portfolio/editar_licenciatura.html', {'licenciatura': licenciatura})
